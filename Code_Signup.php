13b47bfa2544c93eff17655c0c55f77779b3a794
gjlam95 <gj_lam@hotmail.com>
2016-08-04 11:15:16 +0800

    Update and rename Code_Signup.html to Code_Signup.php

--- Code_Signup.php
+++ Code_Signup.php
@@ -116,12 +116,12 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
     <h4 class="w3-padding-0"><b>Minystery</b></h4>
     <p class="w3-text-grey">Series of mini-games</p>
   </div>
-  <a href="index.html" class="w3-padding">Home</a>
-  <a href="Code_Login.html" class="w3-padding w3-text-teal">Log In</a>
-  <a href="Code_Detective.html" class="w3-padding">Detective</a>
-  <a href="Code_Mathematics.html" class="w3-padding">Mathematics</a>
-  <a href="Code_Guessing.html" class="w3-padding">Guessing</a>
-  <a href="Code_Miscellaneous.html" class="w3-padding">Miscellaneous</a>
+  <a href="index.php" class="w3-padding">Home</a>
+  <a href="Code_Login.php" class="w3-padding w3-text-teal">Log In</a>
+  <a href="Code_Detective.php" class="w3-padding">Detective</a>
+  <a href="Code_Mathematics.php" class="w3-padding">Mathematics</a>
+  <a href="Code_Guessing.php" class="w3-padding">Guessing</a>
+  <a href="Code_Miscellaneous.php" class="w3-padding">Miscellaneous</a>
    
 </nav>
 
