58cf0571c3ced6d2bfee4c0fa77d94de3ab413de
gjlam95 <gj_lam@hotmail.com>
2016-08-04 11:12:13 +0800

    Update and rename Code_Detective.html to Code_Detective.php

--- Code_Detective.php
+++ Code_Detective.php
@@ -21,12 +21,12 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
     <h4 class="w3-padding-0"><b>Minystery</b></h4>
     <p class="w3-text-grey">Series of mini-games</p>
   </div>
-  <a href="index.html" class="w3-padding">Home</a>
-  <a href="Code_Login.html" class="w3-padding">Log In</a>
-  <a href="Code_Detective.html" class="w3-padding w3-text-teal">Detective</a>
-  <a href="Code_Mathematics.html" class="w3-padding">Mathematics</a>
-  <a href="Code_Guessing.html" class="w3-padding">Guessing</a>
-  <a href="Code_Miscellaneous.html" class="w3-padding">Miscellaneous</a>
+  <a href="index.php" class="w3-padding">Home</a>
+  <a href="Code_Login.php" class="w3-padding">Log In</a>
+  <a href="Code_Detective.php" class="w3-padding w3-text-teal">Detective</a>
+  <a href="Code_Mathematics.php" class="w3-padding">Mathematics</a>
+  <a href="Code_Guessing.php" class="w3-padding">Guessing</a>
+  <a href="Code_Miscellaneous.php" class="w3-padding">Miscellaneous</a>
    
 </nav>
 
@@ -48,27 +48,27 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
       <img src="http://2.bp.blogspot.com/-oAIUcswqVuU/T47lNW8ZseI/AAAAAAAABgw/IOd0UxCmApc/s1600/Clue-Evidence.jpg" alt="Clue" style="width:85%" class="w3-hover-opacity"><br><br><br>
       <div class="w3-container w3-white">
         <p><b>Level 1</b></p>
-        <p><a href="1_bomb.html">The Gift</a></p>
-	<p><a href="1_ghost.html">The Ghost of Mrs Basil</a></p>
-	<p><a href="1_hide.html">Hide & Seek</a></p>
+        <p><a href="1_bomb.php">The Gift</a></p>
+	<p><a href="1_ghost.php">The Ghost of Mrs Basil</a></p>
+	<p><a href="1_hide.php">Hide & Seek</a></p>
       </div>
     </div>
     <div class="w3-third w3-container w3-margin-bottom">
       <img src="http://static1.squarespace.com/static/5253d671e4b09b5bd4077ed2/t/5530fc6ae4b097a445f1636d/1429273708635/" alt="Clue" style="width:62%" class="w3-hover-opacity">
       <div class="w3-container w3-white">
         <p><b>Level 2</b></p>
-        <p><a href="2_murder.html">The Planned Murder</a></p>
-	<p><a href="2_music.html">The Unfortunate Music Party</a></p>
-	<p><a href="2_basil.html">Death of Mr Basil</a></p>
+        <p><a href="2_murder.php">The Planned Murder</a></p>
+	<p><a href="2_music.php">The Unfortunate Music Party</a></p>
+	<p><a href="2_basil.php">Death of Mr Basil</a></p>
       </div>
     </div>
     <div class="w3-third w3-container">
       <img src="http://img01.deviantart.net/301e/i/2012/351/b/b/clue___weapons_by_virtualbarata-d5oa62f.jpg" alt="Clue" style="width:60%" class="w3-hover-opacity">
       <div class="w3-container w3-white">
         <p><b>Level 3</b></p>
-        <p><a href="3_april.html">April Fools</a></p>
-	      <p><a href="3_fire.html">The Fire</a></p>
-	      <p><a href="3_film.html">Rolls of Film</a></p>
+        <p><a href="3_april.php">April Fools</a></p>
+	      <p><a href="3_fire.php">The Fire</a></p>
+	      <p><a href="3_film.php">Rolls of Film</a></p>
       </div>
     </div>
   </div>
