6600ef3c3b4ad16567536f385da79ab21892bc1c
gjlam95 <gj_lam@hotmail.com>
2016-08-12 22:32:54 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,1 +1,12 @@
+<?php
+include ("dbconnect.php");
 
+session_unset();
+session_destroy();
+
+?>
+
+<script type="text/javascript">
+alert("You are logged out!\nThank You for playing Minystery! :)");
+window.location="index.php";
+</script>
